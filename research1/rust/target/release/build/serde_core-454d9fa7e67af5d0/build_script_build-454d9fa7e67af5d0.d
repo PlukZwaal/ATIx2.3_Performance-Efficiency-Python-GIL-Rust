ca7504f8802eb68ca39309c36ba37aa8b7e795db
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2026-03-17 17:41:28 +0100

    Delete research1/rust/target directory

--- research1/rust/target/release/build/serde_core-454d9fa7e67af5d0/build_script_build-454d9fa7e67af5d0.d
+++
@@ -1,5 +0,0 @@
-C:\Code\ATIx2.3_Performance-Efficiency-Python-GIL-Rust\research1\rust\target\release\build\serde_core-454d9fa7e67af5d0\build_script_build-454d9fa7e67af5d0.d: C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs
-
-C:\Code\ATIx2.3_Performance-Efficiency-Python-GIL-Rust\research1\rust\target\release\build\serde_core-454d9fa7e67af5d0\build_script_build-454d9fa7e67af5d0.exe: C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs
-
-C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs:
--- research1/rust/target/release/multi_thread.d
+++
@@ -1,1 +0,0 @@
-C:\Code\ATIx2.3_Performance-Efficiency-Python-GIL-Rust\research1\rust\target\release\multi_thread.exe: C:\Code\ATIx2.3_Performance-Efficiency-Python-GIL-Rust\research1\rust\src\bin\multi_thread.rs
