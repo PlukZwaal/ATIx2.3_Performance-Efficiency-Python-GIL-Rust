c171470f1f796302bfd39b43e612c4a55e1ab896
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2026-03-17 18:00:19 +0100

    Removed rust files

--- research1/rust/target/release/deps/csv_core-85c2d573a2636b5c.d
+++
@@ -1,9 +0,0 @@
-C:\Code\ATIx2.3_Performance-Efficiency-Python-GIL-Rust\research1\rust\target\release\deps\csv_core-85c2d573a2636b5c.d: C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\lib.rs C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\reader.rs C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\writer.rs
-
-C:\Code\ATIx2.3_Performance-Efficiency-Python-GIL-Rust\research1\rust\target\release\deps\libcsv_core-85c2d573a2636b5c.rlib: C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\lib.rs C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\reader.rs C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\writer.rs
-
-C:\Code\ATIx2.3_Performance-Efficiency-Python-GIL-Rust\research1\rust\target\release\deps\libcsv_core-85c2d573a2636b5c.rmeta: C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\lib.rs C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\reader.rs C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\writer.rs
-
-C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\lib.rs:
-C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\reader.rs:
-C:\Users\plukk\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\csv-core-0.1.13\src\writer.rs:
--- research1/rust/target/release/single_thread.d
+++
@@ -1,1 +0,0 @@
-C:\Code\ATIx2.3_Performance-Efficiency-Python-GIL-Rust\research1\rust\target\release\single_thread.exe: C:\Code\ATIx2.3_Performance-Efficiency-Python-GIL-Rust\research1\rust\src\bin\single_thread.rs
